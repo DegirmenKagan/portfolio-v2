1b8b3184cbc92b5b8030ea186380d39de57475af
Kağan Değirmen <kagandegirmen@gmail.com>
2023-11-13 01:03:59 +0300

    npm, Sidebar, Links, ToggleButton implementation

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./navbar.scss";
 import { Constants } from "../../Constants";
+import { motion } from "framer-motion";
+import Sidebar from "../sidebar/Sidebar";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/* Sidebar */}
+      <Sidebar />
       <div className="wrapper">
-        <span>degirmenkagan</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          degirmenkagan
+        </motion.span>
         <div className="social">
           <a href={Constants.instagram} target="_blank">
             <img src="/instagram.png" alt="instagram" />
---
+++ src/components/sidebar/links/Links.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Links = () => {
+  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+  return (
+    <div className="links">
+      {items.map((item) => (
+        <a href={`#${item}`} key={item}>
+          {item}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default Links;
