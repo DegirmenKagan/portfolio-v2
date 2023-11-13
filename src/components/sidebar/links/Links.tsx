5c827194d1464bb0eed7c78007dd56e382879796
Kağan Değirmen <kagandegirmen@gmail.com>
2023-11-13 19:55:19 +0300

    Sidebar > Link animations

--- src/components/sidebar/links/Links.tsx
+++ src/components/sidebar/links/Links.tsx
@@ -1,15 +1,48 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.1,
+      staggerDistance: -1,
+    },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
 
 const Links = () => {
   const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
