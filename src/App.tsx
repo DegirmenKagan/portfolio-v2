441f753c1c94be0563eb03aae3c304ba1c3f3b41
Kağan Değirmen <kagandegirmen@gmail.com>
2023-11-13 20:44:18 +0300

    Hero component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
 
 function App() {
   return (
     <div>
       <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
       <section id="Services">Parallax</section>
       <section>Services</section>
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import "./hero.scss";
+import { Variants, motion } from "framer-motion";
+
+const textVariants: Variants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+const sliderVariants: Variants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 20,
+    },
+  },
+};
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>KAĞAN DEĞİRMEN</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Frontend developer and Youtube content creator
+          </motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              See the Latest Works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt="scrollIcon"
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Developer Content Creator Photographer Artist
+      </motion.div>
+
+      <div className="imageContainer">
+        <img src="/hero.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
