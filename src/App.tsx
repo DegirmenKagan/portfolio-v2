4cef953069cdf0468a285a0e664ccc3f3eab9678
Kağan Değirmen <kagandegirmen@gmail.com>
2023-11-18 16:32:39 +0300

    responsive Part 3: Parallax

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,15 @@ function App() {
         <Navbar />
         <Hero />
       </section>
-      {/* <section id="Services">
+      <section id="Services">
         <Parallax type="services" />
-      </section> */}
+      </section>
       {/* <section>
         <Services />
       </section> */}
-      {/* <section id="Portfolio">
+      <section id="Portfolio">
         <Parallax type="portfolio" />
-      </section> */}
+      </section>
       {/* <Portfolio />
       <section id="Contact">
         <Contact />
